446927130e13defdffcd321165a6a9c320b4544a
Sam Brown <sbrown2594@gmail.com>
2012-08-15 14:34:52 -0400

    Initial commit.

---
+++ leadspend.jsonp-2.1.js
@@ -0,0 +1,32 @@
+/* LeadSpend Validation Javascript V2.1
+ *
+ * Dependencies:
+ * 	jQuery: https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+ *		jQuery-JSONP: https://github.com/jaubourg/jquery-jsonp
+ */
+
+if (typeof LeadSpend == 'undefined') {
+
+   /* Establish namespace for future use */
+   var LeadSpend = new function() {
+		
+      var subdomain = "primary";
+		
+		/* Expose validation function */
+		this.validate = function(emailAddress, timeoutInSeconds, callback) {
+			jQuery.jsonp({
+				emailAddress: emailAddress,
+				url: "https://" + subdomain + ".api.leadspend.com/v2/validity/" + encodeURIComponent(emailAddress) + "?timeout=" + timeoutInSeconds,
+				callbackParameter: "callback",
+				timeout: timeoutInSeconds * 1000,
+				success: function(data, status) { callback(data); },
+				error: function(xOptions, textStatus) {
+					callback({
+						address: xOptions.emailAddress,
+						result: "unknown"
+					});
+				}
+			});
+		};
+	};
+}
